cea98c4b5aa950ef897768c42bc684ad3ccacd27
soboli89 <84281763+soboli89@users.noreply.github.com>
2024-07-22 15:52:51 -0400

    received data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+//import Itemcomponent from '.components/Itemcomponent';
+
+
+
+
+class App extends Component {
+    
+    state ={
+        isLoading: true,
+        dogs: []
+    };
+    getDogs = async () => {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        console.log(apiKey);
+        const dogs = await axios.get(`https://api.thedogapi.com/v1/images/search?limit=20&api_key=${apiKey}`);
+       // console.log(dogs.data);
+       this.setState({dogs: dogs.data, isLoading: false});
+    }
+    
+    componentDidMount() {
+        this.getDogs();
+}
+   
+    render(){
+        const {isLoading} = this.state;
+        return (
+            <div>{isLoading ? "Loading..." : "The page is loaded"}</div>
+        )
+    }
 }
 
+
+
+
+
+
+
+/*
+function App() {
+   // fetch(`https://api.thedogapi.com/v1/images/search?limit=20&api_key=${API_KEY}`)
+    //.then (res=>res.json())
+    //.then((data) => {
+    //console.log(data);
+    });
+    return ( 
+        <div className = "App" >
+            
+        </div>
+    );
+}
+*/
 export default App;
